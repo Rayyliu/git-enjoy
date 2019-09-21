8e4241e83fa92805b93506a18f0a3bb0211faa2d
liurui <466088269@qq.com>
2019-09-21 22:39:54 +0800

    V 0.1

--- src/main/java/com/gitenjoy/Example.java
+++ src/main/java/com/gitenjoy/Example.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
     @RequestMapping
     String home(){
-        return "hello Git";
+        return "hello Git V0.1";
     }
 
     public static void main(String[] args) {
